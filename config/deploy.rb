4259ff3bebe486ebc3d96362dfdc34cfa101bc22
Ekant <ekantmate@gmail.com>
2019-06-12 06:42:56 +0000

    Set up Puma, Nginx & Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # Change these
-server '54.197.10.24', port: 7171, roles: [:web, :app, :db], primary: true
+server '54.197.10.24', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:ekant-test/testapp_rails.git'
 set :application,     'testapp'
